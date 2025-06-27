a823335d0f8ba3ebfcb00a5f01bc74844f5d4ff3
Martin Kulev <martindkulev@gmail.com>
2025-06-27 11:37:39 +0300

    More log configs

--- MartinKulev/Program.cs
+++ MartinKulev/Program.cs
@@ -9,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddTransient<IProjectsService, ProjectsService>();
+
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+builder.Logging.ClearProviders(); // remove default providers
+builder.Logging.AddSerilog();
 
 builder.Configuration.AddJsonFile(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "GitSecrets.json"), optional: true);
 string? connectionString = builder.Configuration.GetConnectionString("MartinKulev") ?? builder.Configuration.GetConnectionString("DefaultConnection")!;
@@ -25,6 +28,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+Log.Logger.Warning($"Hosting_Environemnt: {app.Environment.EnvironmentName}");
+Log.Logger.Warning($"fhgfhkhjkgede34646");
 
 app.UseHttpsRedirection();
 
