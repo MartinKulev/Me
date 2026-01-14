6efe5807f7078bacdca735a30531a88cdcd927da
Martin Kulev <martindkulev@gmail.com>
2026-01-14 17:21:26 +0200

    Added Education pages and improved existing WorkExperience pages

---
+++ MartinKulev/Services/Helper/HelperService.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace MartinKulev.Services.Helper
+{
+    public static class HelperService
+    {
+        public static string GetDurationToString(DateTime startDate, DateTime? endDate)
+        {
+            if(endDate == null)
+            {
+                endDate = DateTime.Now;
+            }
+
+            int totalMonths = ((endDate.Value.Year - startDate.Year) * 12) + endDate.Value.Month - startDate.Month + 1;
+            if (endDate.Value.Day < startDate.Day)
+            {
+                totalMonths--; // Adjust for partial month
+            }
+
+            int years = totalMonths / 12;
+            int months = totalMonths % 12;
+
+            var parts = new List<string>();
+            if (years > 0) parts.Add($"{years}y");
+            if (months > 0) parts.Add($"{months}m");
+
+            return $"({string.Join(", ", parts)})";
+        }
+
+        public static string GetEndDateToString(DateTime? endDate)
+        {
+            if (endDate == null)
+            {
+                return "Present";
+            }
+
+            return endDate.Value.ToString("MMM yyyy", CultureInfo.InvariantCulture);
+        }
+    }
+}
